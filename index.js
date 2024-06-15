6adcda6ce6c23c11ed9914f8acb2f67277d9e17b
pascal <pas111cal3@gmail.com>
2024-06-15 10:34:12 +0100

    version 1

---
+++ index.js
@@ -0,0 +1,11 @@
+let sideBar = document.querySelector('.side-bar')
+
+function showSideBar() {
+   sideBar.style.display = 'flex'
+}
+
+
+
+function closeSideBar() {
+    sideBar.style.display = 'none'
+}
